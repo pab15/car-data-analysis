255d39ff124ad48fcdda677bbcbf9985a89932c7
pab15 <pab15@humboldt.edu>
2020-03-09 23:26:17 -0700

    Added Notebook, Shaped Data

--- datacollector.py
+++ datacollector.py
@@ -107,7 +107,147 @@ def write_paginated_links():
                     counter += int(count_by)
     f.close()
 
+def build_db():
+    conn = sqlite3.connect('cities.db')
+    curs = conn.cursor()
+    curs.execute('''CREATE TABLE IF NOT EXISTS listings
+                    (id INTEGER PRIMARY KEY, url STRING, price INTEGER, location_pop INTEGER)''')
+    count = 1
+    with open('paglinks.txt') as paglinks:
+        for line in enumerate(paglinks):
+            if line != '':
+                url = line[1].strip('\n')
+                result = []
+                response = urllib.request.urlopen(url)
+                html = response.read()
+                soup = BeautifulSoup(html, 'html.parser')
+                listings = soup.find_all('li', attrs={"class" : "result-row"})
+                for listing in listings:
+                    listing_url = str(listing.find('a'))
+                    listing_url = listing_url.split('"')
+                    try:
+                        listing_url = listing_url[5]
+                    except:
+                        for value in listing_url:
+                            if 'https://' in value:
+                                listing_url = value
+                    else:
+                        curs.execute("""SELECT url FROM "listings"
+                                                WHERE url=?""",
+                                                (listing_url,))
+                        result_url = curs.fetchone()
+                        if result_url:
+                            pass
+                        else:
+                            try:
+                                listing_location = str(listing.find('span', attrs = {"class" : "result-hood"}).string)
+                            except:
+                                pass
+                            else:
+                                if listing_location != '':
+                                    listing_location = str(listing.find('span', attrs = {"class" : "result-hood"}).string)
+                                    listing_location = listing_location.strip(')')
+                                    listing_location = listing_location[2:]
+                                    if '/' in listing_location:
+                                        listing_location = listing_location.split('/')
+                                        listing_location = listing_location[0]
+                                        listing_location = listing_location.split()
+                                        for x in range(len(listing_location)):
+                                            first_letter = listing_location[x][0].upper()
+                                            rest = listing_location[x][1:].lower()
+                                            listing_location[x] = str(first_letter) + str(rest)
+                                        sep = ' '
+                                        listing_location = sep.join(listing_location)
+                                    if '(' in listing_location or ')' in listing_location:
+                                        listing_location = listing_location.split('(', 1)[0]
+                                    listing_location = listing_location.split(' ')
+                                    for x in range(len(listing_location)):
+                                        if x != '' or ' ':
+                                            try:
+                                                first_letter = listing_location[x][0].upper()
+                                                rest = listing_location[x][1:].lower()
+                                                listing_location[x] = str(first_letter) + str(rest)
+                                            except:
+                                                pass
+                                            else:
+                                                first_letter = listing_location[x][0].upper()
+                                                rest = listing_location[x][1:].lower()
+                                                listing_location[x] = str(first_letter) + str(rest)
+                                    sep = ' '
+                                    listing_location = sep.join(listing_location)
+                                    if ',' in listing_location:
+                                        listing_location = listing_location.split(',', 1)[0]
+                                    if ' ca' in listing_location:
+                                        listing_location = listing_location.replace(' ca', '')
+                                    if ' Ca' in listing_location:
+                                        listing_location = listing_location.replace(' Ca', '')
+                                    if ' CA' in listing_location:
+                                        listing_location = listing_location.replace(' CA', '')
+                                    if '.ca' in listing_location:
+                                        listing_location = listing_location.replace('.ca', '')
+                                    if 'SW ' in listing_location:
+                                        listing_location = listing_location.replace('SW ', '')
+                                    if ' SW' in listing_location:
+                                        listing_location = listing_location.replace(' SW', '')
+                                    if 'SW' in listing_location:
+                                        listing_location = listing_location.replace('SW', '')
+                                    if 'Sw ' in listing_location:
+                                        listing_location = listing_location.replace('Sw ', '')
+                                    if ' Sw' in listing_location:
+                                        listing_location = listing_location.replace(' Sw', '')
+                                    if 'Sw' in listing_location:
+                                        listing_location = listing_location.replace('Sw', '')
+                                    if 'sw ' in listing_location:
+                                        listing_location = listing_location.replace('sw ', '')
+                                    if ' sw' in listing_location:
+                                        listing_location = listing_location.replace(' sw', '')
+                                    if 'sw' in listing_location:
+                                        listing_location = listing_location.replace('sw', '')
+                                    if 'NW ' in listing_location:
+                                        listing_location = listing_location.replace('NW ', '')
+                                    if ' NW' in listing_location:
+                                        listing_location = listing_location.replace(' NW', '')
+                                    if 'NW' in listing_location:
+                                        listing_location = listing_location.replace('NW', '')
+                                    if 'Nw ' in listing_location:
+                                        listing_location = listing_location.replace('Nw ', '')
+                                    if ' Nw' in listing_location:
+                                        listing_location = listing_location.replace(' Nw', '')
+                                    if 'Nw' in listing_location:
+                                        listing_location = listing_location.replace('Nw', '')
+                                    if 'nw ' in listing_location:
+                                        listing_location = listing_location.replace('nw ', '')
+                                    if ' nw' in listing_location:
+                                        listing_location = listing_location.replace(' nw', '')
+                                    if 'nw' in listing_location:
+                                        listing_location = listing_location.replace('nw', '')
+                                    if ' and ' in listing_location:
+                                        listing_location = listing_location.split(' and ')
+                                        listing_location = listing_location[0]
+                                    curs.execute("""SELECT population FROM "city"
+                                                    WHERE city=?""",
+                                                    (listing_location,))
+                                    result = curs.fetchone()
+                                    if result: 
+                                        location_pop = int(result[0])
+                                        listing_price = str(listing.find('span', attrs = {"class" : "result-price"}).string)
+                                        listing_price = listing_price.replace('$', '')
+                                        if listing_price != '' and listing_price != '0' and listing_price != '1':
+                                            try:
+                                                listing_price = int(listing_price)
+                                            except:
+                                                pass
+                                            else: 
+                                                listing_price = int(listing_price)
+                                        query = ('''INSERT INTO "listings"
+                                                    VALUES (?,?,?,?)''')
+                                        query_data = (count, listing_url, listing_price, location_pop)
+                                        curs.execute(query, query_data)
+                                        conn.commit()
+                                        count += 1
+    conn.close()
+
 if __name__ == '__main__':
     # write_population_data()
     # write_to_db('popdatabycity.csv')
-    write_paginated_links()
+    build_db()
